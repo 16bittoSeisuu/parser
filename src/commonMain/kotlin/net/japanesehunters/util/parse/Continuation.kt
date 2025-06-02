d3960d740fce974f1bcedfcdd57e54c64fe295cf
Int16 <integer16bit@gmail.com>
2025-06-02 11:03:22 +0900

    add `remainder` property for `Ok` and make `fold` gives Continuation itself

--- src/commonMain/kotlin/net/japanesehunters/util/parse/Continuation.kt
+++ src/commonMain/kotlin/net/japanesehunters/util/parse/Continuation.kt
@@ -12,18 +12,25 @@ sealed interface Continuation<
 sealed interface Ok<out Tok : Any, out Res> :
   Continuation<Tok, Nothing, Res> {
   val result: Res
+  val remainder: Cursor<Tok>
+
+  operator fun component1() = result
+
+  operator fun component2() = remainder
 }
 
 data class Cont<out Tok : Any, out Res>(
   override val result: Res,
-  val remainder: Zipper<Tok>,
+  override val remainder: Zipper<Tok>,
 ) : Ok<Tok, Res>
 
 data class Done<out Tok : Any, out Res>(
   override val result: Res,
-  val cursor: Cursor<Tok>,
+  override val remainder: Cursor<Tok>,
 ) : Ok<Tok, Res>
 
 value class Err<out Err>(
   val error: Err,
-) : Continuation<Nothing, Err, Nothing>
+) : Continuation<Nothing, Err, Nothing> {
+  operator fun component1() = error
+}
--- src/commonMain/kotlin/net/japanesehunters/util/parse/ContinuationExtensions.kt
+++ src/commonMain/kotlin/net/japanesehunters/util/parse/ContinuationExtensions.kt
@@ -1,20 +1,17 @@
 package net.japanesehunters.util.parse
 
-import net.japanesehunters.util.collection.Cursor
-import net.japanesehunters.util.collection.Zipper
-
 inline fun <
   T : Any,
   E,
   R,
   U,
   > Continuation<T, E, R>.fold(
-  onOk: (R) -> U,
-  onErr: (E) -> U,
+  onOk: (Ok<T, R>) -> U,
+  onErr: (Err<E>) -> U,
 ): U =
   when (this) {
-    is Ok -> onOk(result)
-    is Err -> onErr(error)
+    is Ok -> onOk(this)
+    is Err -> onErr(this)
   }
 
 inline fun <
@@ -23,14 +20,14 @@ inline fun <
   R,
   U,
   > Continuation<T, E, R>.fold(
-  onDone: (R, Cursor<T>) -> U,
-  onCont: (R, Zipper<T>) -> U,
-  onErr: (E) -> U,
+  onDone: (Done<T, R>) -> U,
+  onCont: (Cont<T, R>) -> U,
+  onErr: (Err<E>) -> U,
 ): U =
   when (this) {
-    is Done -> onDone(result, cursor)
-    is Cont -> onCont(result, remainder)
-    is Err -> onErr(error)
+    is Done -> onDone(this)
+    is Cont -> onCont(this)
+    is Err -> onErr(this)
   }
 
 inline fun <
@@ -41,22 +38,21 @@ inline fun <
   > Continuation<T, E, R1>.map(
   onOk: (R1) -> R2,
 ): Continuation<T, E, R2> =
-  when (this) {
-    is Done -> Done(onOk(result), cursor)
-    is Cont -> Cont(onOk(result), remainder)
-    is Err -> this
-  }
+  fold(
+    { (res, cur) -> Done(onOk(res), cur) },
+    { (res, zip) -> Cont(onOk(res), zip) },
+    { it },
+  )
 
 inline fun <
   T : Any,
-  C : Any,
   E1,
   E2,
   R,
   > Continuation<T, E1, R>.mapErr(
   onOk: (E1) -> E2,
 ): Continuation<T, E2, R> =
-  when (this) {
-    is Ok -> this
-    is Err -> Err(onOk(error))
-  }
+  fold(
+    { it },
+    { (err) -> Err(onOk(err)) },
+  )
